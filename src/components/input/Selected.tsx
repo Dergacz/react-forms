ef316993d74f9d6bc11407e6fa58cf614756dc73
Dergacz <sats96dergach@gmail.com>
2021-04-10 13:43:35 +0200

    fix selected

--- src/components/input/Selected.tsx
+++ src/components/input/Selected.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {KeyboardEventHandler, useEffect, useState} from "react";
 import style from "./Selected.module.css"
 
 
@@ -25,25 +25,54 @@ export const ControledSelected = (props: SelectedRatingType) => {
     const selectItem = props.items.find(i => i.value === props.value);
     const hoveredItem = props.items.find(i => i.value === hoveredElement);
 
+    useEffect(() => {
+        setHoveredElement(props.value);
+
+    }, [props.value])
+
+    const onKeyUp = (e: any) => {
+        if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+            for (let i = 0; i < props.items.length; i++) {
+                if (props.items[i].value === hoveredElement) {
+                    const pretendentElement = e.key === "ArrowDown" ? props.items[i + 1] : props.items[i - 1];
+                    if (pretendentElement) {
+                        props.onChange(pretendentElement.value);
+                        return;
+                    }
+                }
+            }
+            if (!selectItem) {
+                props.onChange(props.items[0].value)
+            }
+        }
+
+        if (e.key === "Enter" || e.key === "Escape") {
+            setActive(false)
+        }
+    }
 
 
     return (
         <div>
-            <div className={style.select}>
+            <div className={style.select}
+                 onKeyUp={onKeyUp}
+                 tabIndex={0}
+            >
                 <span className={style.main} onClick={showSelect}>{selectItem && selectItem.title}</span>
                 {active &&
                 <div className={style.items}>
                     {props.items.map(i => {
-                        
+
                         return <div
+
                             className={style.item + " " + (hoveredItem === i ? style.selected : "")}
                             key={i.value}
-                            onClick={() =>{
+                            onClick={() => {
                                 clickSelectElement(i.value)
                             }}
                             onMouseEnter={() => setHoveredElement(i.value)}
                         >{i.title}</div>
-                     })}
+                    })}
                 </div>
                 }
 
