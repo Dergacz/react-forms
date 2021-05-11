c807ade39809dce78cbde3f923b44b32734bb3e2
Dergacz <sats96dergach@gmail.com>
2021-05-11 17:12:52 +0200

    add useEffect.stories

---
+++ src/stories/UseEffect.stories.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect, useMemo, useState} from "react";
+
+
+export default {
+    title: "UseEffect"
+}
+
+export const SimpleExample = () => {
+    const [fake, setFake] = useState(1);
+    const [counter, setCounter] = useState(1);
+    console.log("SimpleExample");
+
+    useEffect(() => {
+        console.log("UseEffect every render");
+        document.title = counter.toString();
+    });
+
+    useEffect(() => {
+        console.log("UseEffect only first render");
+        document.title = counter.toString();
+    }, []);
+
+    useEffect(() => {
+        console.log("UseEffect only first render and every counter change");
+        document.title = counter.toString();
+    }, [counter]);
+
+    return (
+        <div>
+            Hello, {counter} {fake}
+            <button onClick={() => setFake(fake + 1)}>fake+</button>
+            <button onClick={() => setCounter(counter + 1)}>counter+</button>
+        </div>
+    )
+}
+
