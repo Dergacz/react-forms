c5b30942376713b13fa02dcd1c2ef19d52e3214e
Dergacz <sats96dergach@gmail.com>
2021-06-22 18:12:25 +0200

    fix clock with clearInterval

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -4,9 +4,13 @@ export const Clock = () => {
     const [date, setDate] = useState(new Date());
 
     useEffect(() => {
-        setInterval(() => {
+       const intervalId = setInterval(() => {
             setDate(new Date())
         }, 1000)
+
+        return () => {
+           clearInterval(intervalId);
+        }
     }, []);
 
     const zeroTime = (num: number) => {
